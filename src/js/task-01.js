f2796351e442f70a56a6c67e77827192b781c6b7
AllaVoitiuk <allavoytjuk@ukr.net>
2022-10-15 20:06:01 +0300

    update task1-js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -9,22 +9,32 @@
 // Category: Technologies
 // Elements: 5
 
-const items = document.querySelectorAll('.item');
-console.log('');
-console.log(`Number of categories: ${items.length}`);
-console.log('');
-items.forEach((item)=>{
-    const group = [...item.children];
+// const items = document.querySelectorAll('.item');
+// console.log('');
+// console.log(`Number of categories: ${items.length}`);
+// console.log('');
+// items.forEach((item)=>{
+//     const group = [...item.children];
 
-    group.forEach(element => {
-    if (element.tagName === 'H2') {
-        console.log(`Elements: ${element.textContent}`);
-    }
-});
-group.forEach(element => {
-    if (element.tagName === 'UL') {
-        console.log(`Elements: ${element.children.length}`);
-    }
-});
-console.log(' ');
+//     group.forEach(element => {
+//     if (element.tagName === 'H2') {
+//         console.log(`Elements: ${element.textContent}`);
+//     }
+// });
+// group.forEach(element => {
+//     if (element.tagName === 'UL') {
+//         console.log(`Elements: ${element.children.length}`);
+//     }
+// });
+// console.log(' ');
+// });
+
+
+const list = document.querySelector('#categories');
+const items = list.querySelectorAll('.item');
+console.log(`В списке ${items.length} категории:`);
+items.forEach(node => {
+const h2 = node.querySelector('h2');
+const nodeItems = node.querySelectorAll('li');
+console.log(` - категория: ${h2.textContent} (количество элементов: ${nodeItems.length})`);
 });
